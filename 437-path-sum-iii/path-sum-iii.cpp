6094d23c8accb2ab66fdc0b8305d327885957622
harshdixit22 <131592907+harshdixit22@users.noreply.github.com>
2025-06-22 13:13:18 +0530

    Time: 24 ms (12.62%) | Memory: 18.7 MB (99.29%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,46 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-// class Solution {
-//     int count;
-// public:
 
-
-//     bool solve(TreeNode* root, int targetSum){
-//         if(root==NULL){
-//             return false;
-//         }
-
-//         if(targetSum==0){
-//             return true;
-//         }
-
-//         return solve(root->left , targetSum - (root->val))  || solve(root->right , targetSum - (root->val));
-        
-        
-//     }
-//     int pathSum(TreeNode* root, int targetSum) {
-
-//         if(root==NULL){
-//             return 0;
-//         }
-//         int z = z +  solve(root,targetSum) == true ? 1 :0;
-//         int x = x+ solve(root->left,targetSum)== true ? 1 :0;
-//         int y =y+  solve(root->right,targetSum)== true ? 1 :0;
-
-//         return x + y + z;
-        
-        
-//     }
-// };
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
