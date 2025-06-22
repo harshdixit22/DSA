7d30f376d9b026a2bf03e0586ce09beb8ed47f59
harshdixit22 <131592907+harshdixit22@users.noreply.github.com>
2025-06-22 11:29:45 +0530

    Update binary-tree-paths.cpp

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+-------------------------------------------- when string is passed by value --------------------------------------------
 class Solution {
 public:
     void dfs(TreeNode* node,vector<string> &res,string curr){
@@ -40,3 +42,73 @@ public:
         return res;
     }
 };
+
+
+----------------------------------------- when string is passed by reference -----------------------------------------
+class Solution {
+public:
+    void dfs(TreeNode* node,vector<string> &res,string curr){
+        if(node==NULL){
+            return;
+        }
+        int prevSize = curr.size();
+
+
+        if(node->left ==NULL && node->right==NULL){
+            curr+=to_string(node->val);
+            res.push_back(curr);
+        }
+        else{
+            curr+=to_string(node->val) + "->";
+        }
+        dfs(node->left , res , curr);
+
+        dfs(node->right , res,  curr);
+
+        curr.resize(prevSize);
+
+        
+        
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> res;
+        string curr = "";
+
+        dfs(root, res, curr);
+        return res;
+    }
+};
+
+
+------------------------------ Particular node is given -----------------------------------------------------
+
+class Solution {
+public:
+    bool getPath(TreeNode* root, vector<int>& arr, int x) {
+        if (!root)
+            return false;
+
+        arr.push_back(root->val);
+
+        if (root->val == x)
+            return true;
+
+        if (getPath(root->left, arr, x) || getPath(root->right, arr, x))
+            return true;
+
+        arr.pop_back(); // backtrack
+        return false;
+    }
+
+    vector<int> solve(TreeNode* A, int B) {
+        vector<int> arr;
+        if (A == NULL) {
+            return arr;
+        }
+        getPath(A, arr, B);
+        return arr;
+    }
+};
+  
+    
